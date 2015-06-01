a99d080f6f1552295fdc8a384b522f69873ed2d3
Nathan Werbeckes <werbeckes@gmail.com>
2015-06-01 08:54:11 -0500

    will destroy all dependents during all deletes now

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :journey
   has_one :user, through: :journey
-  has_many :notes
+  has_many :notes, dependent: :destroy
   has_many :snippets, through: :notes
 end
